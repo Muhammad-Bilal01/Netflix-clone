7a371931c2bfbfc4d1a7b46a9d3ddcbc35630078
Muhammad Bilal <mbilal254.m@gmail.com>
2023-12-20 15:01:29 +0500

    Navbar Complete Design

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "../../images/netflix-logo.png";
+
+import { Link } from "react-router-dom";
+import { GoSearch } from "react-icons/go";
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <img src={logo} alt="logo" />
+      <div>
+        <Link to="/tvshows">Tv Shows</Link>
+        <Link to="/movies">Movies</Link>
+        <Link to="/dramas">Dramas</Link>
+        <Link to="/mylist">My List</Link>
+      </div>
+      <GoSearch />
+    </nav>
+  );
+};
+
+export default Header;
